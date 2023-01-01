43b3543ca8613aed9c210129cc730aa1c25ba3ec
HuangYilong <hylong0211@qq.com>
2023-01-01 17:18:55 +0800

    fix:添加swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // swagger 文档的配置
+  const option = new DocumentBuilder().setTitle('后台API文档').build();
+  const document = SwaggerModule.createDocument(app, option);
+  SwaggerModule.setup('/api-docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
